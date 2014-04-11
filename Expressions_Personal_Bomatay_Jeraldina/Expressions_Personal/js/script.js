ddca76a501d5b688ee150fd1e9d8557a01c20360
jeraldina-student <jeraldina@fullsail.edu>
2014-04-11 12:48:30 -0400

    Updating homework and assignments

---
+++ Expressions_Personal_Bomatay_Jeraldina/Expressions_Personal/js/script.js
@@ -0,0 +1,30 @@
+/* Made by
+Jeraldina Bomatay
+4/10/2014
+Assignment: Expressions
+ */
+ 
+ 
+ /* FIND BMR  for a healthy weight
+Women: BMR = 655 + ( 4.35 x weight in pounds ) + ( 4.7 x height in inches ) - ( 4.7 x age in years )
+Men: BMR = 66 + ( 6.23 x weight in pounds ) + ( 12.7 x height in inches ) - ( 6.8 x age in year )
+ */
+ 
+var bmr;
+var gender = prompt("Are you male or female?");
+var weight = prompt("How many pounds do you weigh?");
+var height = prompt("What is your height in inches?");
+var age = prompt("What is your age?");
+
+if (gender !== "male" and gender !== "female") 
+var gender = prompt("Are you male or female?");
+}
+
+if ("female" === gender) {
+    bmr = 655+ (4.35 * weight) + (4.7 * height) - (4.7 * age);
+}
+if ("male" === gender) {
+    bmr =  66+ (6.23 * weight) + (12.7 * height) - (6.8 * age); 
+    
+}   
+alert ("Your bmr is " + bmr );
