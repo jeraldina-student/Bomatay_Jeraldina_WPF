b4962b6b0d45872dc0025758964822c80e42ed3d
jeraldina-student <jeraldina@fullsail.edu>
2014-05-14 17:33:18 -0400

    week GoTO meeting

---
+++ week2 go to meetin assignment/js/script.js
@@ -0,0 +1,7 @@
+//create a js file that will tell us how old we are
+//create a variable to hold the person's name
+
+var name= prompt(Type in your name:);
+
+//alert the user
+alert("Welcome "+name+"! We have one more question for you");
