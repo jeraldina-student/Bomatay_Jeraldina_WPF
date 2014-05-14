63e4356ae8bb52099aa1a67e47b8d8740661ad1a
jeraldina-student <jeraldina@fullsail.edu>
2014-05-14 17:36:08 -0400

    ask for yearBorn

--- week2 go to meetin assignment/js/script.js
+++ week2 go to meetin assignment/js/script.js
@@ -5,3 +5,12 @@ var name= prompt(Type in your name:);
 
 //alert the user
 alert("Welcome "+name+"! We have one more question for you");
+
+//Ask user to tell ys year of birth
+//Create variable to hold that year
+
+var yearBorn = prompt("What year were you born?")
+//console log the answer (for us for checking various things)
+
+console.log("You were born in "+yearBorn)
+
