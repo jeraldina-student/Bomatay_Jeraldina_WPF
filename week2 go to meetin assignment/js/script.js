8a3f96efda54c3c95aec65f6e17e2c5194d52e48
jeraldina-student <jeraldina@fullsail.edu>
2014-05-14 17:41:40 -0400

    calculate age and update index.html file

--- week2 go to meetin assignment/js/script.js
+++ week2 go to meetin assignment/js/script.js
@@ -1,6 +1,5 @@
 //create a js file that will tell us how old we are
 //create a variable to hold the person's name
-
 var name= prompt(Type in your name:);
 
 //alert the user
@@ -8,9 +7,16 @@ alert("Welcome "+name+"! We have one more question for you");
 
 //Ask user to tell ys year of birth
 //Create variable to hold that year
+var yearBorn = prompt("What year were you born?";
 
-var yearBorn = prompt("What year were you born?")
 //console log the answer (for us for checking various things)
+console.log("You were born in "+yearBorn+".");
 
-console.log("You were born in "+yearBorn)
+//Current year and subtract the year they were born
+var currentYear = 2014;
 
+//capture the person's age
+var age = currentYear-yearBorn -1;
+
+//alert user of their age
+alert(name + "you are "+age+" years old.");
