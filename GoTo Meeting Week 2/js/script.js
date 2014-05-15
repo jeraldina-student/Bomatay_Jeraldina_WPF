698c35b11595aea23ccaaba1eb7ea7ab0ed5b5c1
jeraldina-student <jeraldina@fullsail.edu>
2014-05-14 21:09:24 -0400

    updating documents

--- GoTo Meeting Week 2/js/script.js
+++ GoTo Meeting Week 2/js/script.js
@@ -1,5 +1,3 @@
-alert("Welcome");
-
 //create a js file that will tell us how old we are
 //create a variable to hold the person's name
 var name = prompt("Type in your name:");
@@ -7,13 +5,11 @@ var name = prompt("Type in your name:");
 //alert the user
 alert("Welcome "+name+"! We have one more question for you.");
 
-
 //Ask user to tell us year of birth
 //Create variable to hold that year
 var yearBorn = prompt("What year were you born?");
 
-
-//console log the answer (for us for checking various things)
+//console log the answer, for us for checking various things
  console.log("You were born in "+yearBorn+".");
 
 //Current year and subtract the year they were born
@@ -23,7 +19,7 @@ var currentYear = 2014;
 var age = currentYear - yearBorn -1;
 
 //alert user of their age
-alert(name+"you are "+age+" years old.");
+alert(name+" you are "+age+" years old.");
 
 //let;s make this mpore complex 
 //how old will you be in the future
@@ -34,4 +30,3 @@ console.log(futureAge);
 
 //make the results "pretty"
 console.log("You will be "+futureAge+" in "+yearsMore+"years."); 
-/*
