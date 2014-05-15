dfdb220ae6d431ac7e05c17d8a8b5de941f2d628
jeraldina-student <jeraldina@fullsail.edu>
2014-05-15 11:21:42 -0400

    create personal directory for project

---
+++ Personal/js/script.js
@@ -0,0 +1,49 @@
+/* Personal - how much gas do you consume on a weekly basis.
+
+- determine gas mileage consumption
+- Find:
+   * car mph    * cost of gas
+   * miles for commute
+   * miles for going to church
+   * miles for going to the gym
+
+how many miles per gallon does your car consume?
+mpg
+
+(gasPrice / mpg )  * weeklyCommute
+
+What is the current rate for gas?
+gasPrice
+
+How many miles do you commute to work on a  weekly basis?
+workMiles
+
+What is the weekly cost of parking
+parking
+
+Enter number of miles to weekly activity?
+miscActivity
+
+workMiles+parking+miscActivity = weeklyCommute */
+
+var gasPrice = prompt("What is the mpg rate for your car?");
+var workMiles = prompt("How many miles do you commute to work on a weekly basis?");
+var parking = prompt("What is the weekly cost of parking?");
+var miscActivity = prompt("Do you have a weekly activity? Please enter the number of miles to said weekly activity. Ex: Church, gym etc..");
+var weeklyCommute = workMiles + parking + miscActivity;
+console.log("You commute "+weeklyCommute+"miles.");
+
+
+var commutePrice = weeklyCommute * (gasPrice/mpg);
+
+alert("You are spending "+commutePrice+" commuting on a weekly basis.");
+console.log("You are spending "+commutePrice+" commuting on a weekly basis.");
+
+
+
+
+
+
+
+
+
