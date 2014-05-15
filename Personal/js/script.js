3bed898832a908ebd678e3a367c39a0a756f2d5a
jeraldina-student <jeraldina@fullsail.edu>
2014-05-15 11:44:30 -0400

    updating math logic

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -5,12 +5,11 @@
    * car mpg   
    * cost of gas
    * miles for commute
+   * parking
    * miles for going to church
 
 how many miles per gallon does your car consume?
 mpg
-
-(gasPrice / mpg )  * weeklyCommute
 
 What is the current rate for gas?
 gasPrice
@@ -23,21 +22,20 @@ parking
 
 Enter number of miles to weekly activity?
 miscActivity
-
-workMiles+parking+miscActivity = weeklyCommute */
+ */
 
 var mpg = prompt("What is the mpg rate for your car?");
 var gasPrice = prompt("What is the current price for gas per gallon");
 var workMiles = prompt("How many miles do you commute to work on a weekly basis?");
 var parking = prompt("What is the weekly cost of parking?");
 var miscActivity = prompt("Do you have a weekly activity? Please enter the number of miles to said weekly activity. Ex: Church, gym etc..");
-var weeklyCommute = workMiles + parking + miscActivity;
-console.log("You commute "+weeklyCommute+"miles.");
+var totalMiles = workMiles + miscActivity;
+console.log("You commute "+totalMiles+" miles and spend $"+parking+" on parking.");
 
 
-var commutePrice = weeklyCommute * (gasPrice/mpg);
+var commutePrice = (totalMiles/mpg) * gasPrice + (parking);
 
-alert("You are spending "+commutePrice+" commuting on a weekly basis.");
+alert("Your total commute spending is "+commutePrice+" on a weekly basis.");
 console.log("You are spending "+commutePrice+" commuting on a weekly basis.");
 
 
