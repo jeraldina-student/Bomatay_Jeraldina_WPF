efd969d5850fd5d2c89008e7da5841d3142f607e
jeraldina-student <jeraldina@fullsail.edu>
2014-05-15 11:25:14 -0400

    adding additional variable

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -26,7 +26,8 @@ miscActivity
 
 workMiles+parking+miscActivity = weeklyCommute */
 
-var gasPrice = prompt("What is the mpg rate for your car?");
+var mpg = prompt("What is the mpg rate for your car?");
+var gasPrice = prompt("What is the current price for gas per gallon");
 var workMiles = prompt("How many miles do you commute to work on a weekly basis?");
 var parking = prompt("What is the weekly cost of parking?");
 var miscActivity = prompt("Do you have a weekly activity? Please enter the number of miles to said weekly activity. Ex: Church, gym etc..");
